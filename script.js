9a14365524d6b5dc8230674b66d245ae66299dbb
Jeremy <jeremyVDLRC@gmail.com>
2020-06-18 00:08:58 +1000

    add pseudocode with basic outline of application

---
+++ script.js
@@ -0,0 +1,10 @@
+//user will have time block from 9am to 5pm in hour increments when load page
+
+// when user clicks on a time block, user can enter their task for that hour
+
+// when user clicks save button, task is saved to localStorage until cleared
+
+//browser will read real time data on current time and dynamically interact with application
+// - if time has already past, time block = grey ;  
+// - if current hour, time block = red;
+// - if hour in future, time block = green;
