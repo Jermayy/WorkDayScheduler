b255097f8903fc9ff8e7e26e6e4ba26967b19b7b
Jeremy <jeremyVDLRC@gmail.com>
2020-06-21 00:29:51 +1000

    added additional pseudo code and display current date/time on page. Linked script.js to index

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //user will have time block from 9am to 5pm in hour increments when load page
 
+// browser will read current day and time and display it above the scheduler      
+
+
 // when user clicks on a time block, user can enter their task for that hour
 
 // when user clicks save button, task is saved to localStorage until cleared
@@ -8,3 +11,11 @@
 // - if time has already past, time block = grey ;  
 // - if current hour, time block = red;
 // - if hour in future, time block = green;
+
+let currentDate = $("#currentDay");
+
+function showCurrentDay() {
+    currentDate.text(moment().format('dddd, MMMM DD YYYY, h:mm:ss a'));
+}
+
+showCurrentDay();
